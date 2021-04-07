6e24725e2b439f60542659072fb2cb96af81ccb7
Andy Wang <qian.wang19@imperial.ac.uk>
2021-04-07 15:35:44 +0100

    Fix remap-std test as CHECK-NOT doesn't seem to work with regex

--- src/test/codegen/remap_path_prefix/issue-73167-remap-std.rs
+++ src/test/codegen/remap_path_prefix/issue-73167-remap-std.rs
@@ -1,15 +1,10 @@
 // ignore-windows
 
-// compile-flags: -g  -C no-prepopulate-passes --remap-path-prefix={{cwd}}/library/std=/the/std
+// compile-flags: -g  -C no-prepopulate-passes --remap-path-prefix=/=/the/root/
 
 // Here we check that imported code from std has their path remapped
-// Ideally we want to do
-// COM: CHECK-NOT: !DIFile(filename: "{{cwd}}/library/std/src/thread/mod.rs"
-// But {{cwd}} will not be expanded here, so we have to check on the bit afterwards
-// Note that we can't use a positive check on `/the/std` either, because the rempap flag will
-// have no effect when `remap-debuginfo` is set to true in `config.toml`, causing the test to fail
 
-// CHECK-NOT: !DIFile(filename: "{{.*}}/library/std"
+// CHECK: !DIFile(filename: "{{/the/root/.*/library/std/src/panic.rs}}"
 fn main() {
     std::thread::spawn(|| {
         println!("hello");
